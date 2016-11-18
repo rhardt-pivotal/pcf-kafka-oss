3604fffadc241e0847bd1523bc84b11c0b357a8e
bbertka-pivotal <bbertka@pivotal.io>
2016-11-18 04:30:42 +0000

    Added Docker Bosh tile build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+cd docker-kafka
+docker build -t rhardt/kafka:latest .
+cd ..
+mkdir docker-cache
+docker save rhardt/kafka | gzip -c > docker-cache/rhardt-kafka.tgz
+tile build --docker-cache ./docker-cache
+pcf import product/*.pivotal
